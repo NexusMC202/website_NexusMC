0699e84c25ef93d282d20ee6903ef0907a3031e1
robloxksergg-creator <robloxksergg@gmail.com>
2026-07-29 09:21:50 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "ebbbe61c-0e71-44ca-9f20-2dd92c120237";
 
 const { d1, r2 } = hostingConfig;
 
